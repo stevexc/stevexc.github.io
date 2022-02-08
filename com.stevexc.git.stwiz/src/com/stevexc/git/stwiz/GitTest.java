9b3378c38f55bfa5ca4df403b71a03e45b8f6357
stevexc <sgmcphail@gmail.com>
2022-02-07 20:40:53 -0700

    Message adjustment

--- com.stevexc.git.stwiz/src/com/stevexc/git/stwiz/GitTest.java
+++ com.stevexc.git.stwiz/src/com/stevexc/git/stwiz/GitTest.java
@@ -2,6 +2,6 @@ package com.stevexc.git.stwiz;
 
 public class GitTest {
 	public static void main(String[] args) {
-	    System.out.println("Git is fun");
+	    System.out.println("Git is cool");
 	  }
 }
